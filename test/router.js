0338dc5d34cc8b76a97abc8cced59759d185726f
Adly Abdullah <adly@trustedcompany.com>
2016-05-27 17:16:24 +0800

    Test routing fairness

--- test/router.js
+++ test/router.js
@@ -5,34 +5,44 @@ var router = require('../lib/router');
 
 describe('Router',function(){
 	it('should return server url',function(){
-		router.routes = {
-			'test': ['testserver:80']
-		}
-		
+		router.routes = {'test': ['testserver:80']};
 		expect(router.route('/test')).to.equal('testserver:80');
 	});
 	
 	it('should handle wildcard url',function(){
-		router.routes = {
-			'test/*': ['testserver:80']
-		}
-		
+		router.routes = {'test/*': ['testserver:80']};
 		expect(router.route('/test/mango/jango')).to.equal('testserver:80');
 	});
 	
 	it('should return undefined if no match',function(){
-		router.routes = {
-			'test': ['testserver:80']
-		}
-		
+		router.routes = {'test': ['testserver:80']}
 		expect(router.route('/mango')).to.be.undefined;
 	});
 	
 	it('should handle default wildcard',function(){
-		router.routes = {
-			'*': ['default:80']
+		router.routes = {'*': ['default:80']};
+		expect(router.route('/mango')).to.equal('default:80');
+	});
+	
+	it('should route fairly',function(){
+		router.routes = {'*': [
+			'default:80',
+			'default:81',
+			'default:82'
+		]};
+		
+		var result = {
+			'default:80': 0,
+			'default:81': 0,
+			'default:82': 0
 		}
 		
-		expect(router.route('/mango')).to.equal('default:80');
+		for (var x=0; x<1000; x++) {
+			result[router.route('/')] ++;
+		}
+		
+		expect(result['default:80']/result['default:81']).to.be.closeTo(1,0.1);
+		expect(result['default:82']/result['default:81']).to.be.closeTo(1,0.1);
+		expect(result['default:80']/result['default:82']).to.be.closeTo(1,0.1);
 	});
 });
